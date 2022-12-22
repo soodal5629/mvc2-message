c6b1068d9b3830278626a17479e9fffed626c5a8
chaelynjang <chaelynjang@lotte.net>
2022-12-22 21:35:39 +0900

    메시지, 국제화 기능 테스트

---
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,57 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+public class MessageSourceTest {
+    @Autowired
+    MessageSource ms; // @SpringBootTest 덕분에 스프링 부트가 돌면서 자동으로 등록해줌
+
+    @Test
+    void helloMessage() {
+        String hello = ms.getMessage("hello", null, null);
+        assertThat(hello).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        // 메시지 못찾을 때 터지는 예외 확인
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void notFoundMessageCodeDefaultMessage() {
+        // 메시지 못찾을 때 디폴트 메시지를 줄 수 있음
+        String result = ms.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() {
+        // 메시지에 파라미터 넘기는 예제
+        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(message).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void defaultLang() {
+        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang() {
+        assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+    }
+}
